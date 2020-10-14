b44c9bb755b56ba36132cc1411e15ada50d0fd26
Ederson Henrique Sá <edersonhs3217@gmail.com.br>
2020-10-14 13:34:04 -0300

    Dia 2

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,50 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// create map
+const map = L.map('mapid', options).setView([-27.8123658,-50.3283711], 15);
+
+// create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create and add icon
+const icon =L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create and add marker
+L
+.marker([-27.8123658,-50.3283711], { icon })
+.addTo(map)
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    // remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    } 
+
+    // selecionar a image clicacada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    // atualizar o container de images
+    imageContainer.src = image.src
+
+    // adicionar a classe .active para este botão
+    button.classList.add("active")
+}
