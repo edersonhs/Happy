6feb047290bea33d3a100354f6e8acf1197091c6
Ederson Henrique Sá <edersonhs3217@gmail.com.br>
2020-10-17 21:14:40 -0300

    Dia 4

---
+++ src/database/fakedata.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.8123658",
+        lng: "-50.3283711",
+        name: "Lar das Meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontrem em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1601564267830-523b71e24db0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1582167410766-a90eea4787ae?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontate e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até as 8h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-27.8123658",
+        lng: "-50.3583711",
+        name: "Lar dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontrem em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1601564267830-523b71e24db0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1582167410766-a90eea4787ae?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontate e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até as 8h",
+        open_on_weekends: "0"
+    },
+]
